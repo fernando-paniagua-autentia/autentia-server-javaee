3c1853056da804902857300ebd12f2bc0b1d91bf
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-04-22 14:54:35 +0200

    Cambio API

--- src/main/java/com/fernandopaniagua/server/controllers/ShareController.java
+++ src/main/java/com/fernandopaniagua/server/controllers/ShareController.java
@@ -22,6 +22,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -67,107 +68,26 @@ public class ShareController implements Serializable {
 	
 	@GET
 	@Path("/byGroup")
-	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getGastosCompartios(Group grupo) {
+	public Response getSharedByGroup(
+			@QueryParam("groupOwnerId") int groupOwnerId) {
 		if (gastoBean==null) {
 			gastoBean = new ShareBean();
 		}
-		List<Share> listaGastos = gastoBean.findAllByIdPropietario(grupo.getIdPropietario());
+		List<Share> listaGastos = gastoBean.findAllByIdPropietario(groupOwnerId);
 		return Response.ok(listaGastos).build();
 	}
 
-//	/**
-//	 * URL: http://localhost:8080/API_REST_WS-RS/api/users/Rosa
-//	 * 
-//	 * @param name String
-//	 * @return Response
-//	 */
 //	@GET
-//	@Path("/{name}")
+//	@Path("/byGroup")
 //	@Consumes(MediaType.APPLICATION_JSON)
 //	@Produces(MediaType.APPLICATION_JSON)
-//	public Response getUserById(@PathParam("name") String name) {
-//		Usuario found = null;
-//		for (int i = 0; i < listaUsuarios.size(); i++) {
-//			if (listaUsuarios.get(i).getNombre().equalsIgnoreCase(name)) {
-//				found = listaUsuarios.get(i);
-//			}
+//	public Response getSharedByGroup(Group grupo) {
+//		if (gastoBean==null) {
+//			gastoBean = new ShareBean();
 //		}
-//		if (found == null) {
-//			return Response.status(Status.BAD_REQUEST).entity("User not found").build();
-//		} else {
-//			return Response.ok(found).build();
-//		}
-//	}
-//
-//	/**
-//	 * URL: http://localhost:8080/API_REST_WS-RS/api/users/createUser Parameters in
-//	 * Postman: {"name":"Rosa3333","username":"Marfi3333l"}
-//	 * 
-//	 * @param User
-//	 * @return Response list NOTA: Si no existe el constructor vacío de User, da un
-//	 *         error y el userRequest viene null.
-//	 */
-//	@POST
-//	@Path("/createUser")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response createUser(Usuario userRequest) {
-//
-//		this.listaUsuarios.add(userRequest);
-//		// return Response.status(Status.CREATED).build();
-//		return Response.ok(listaUsuarios).build();
-//
-//	}
-//
-//	/**
-//	 * URL: http://localhost:8080/API_REST_WS-RS/api/users/updateUser Parameters in
-//	 * Postman: {"name":"Rosa","username":"Marfil3333"}
-//	 * 
-//	 * @param User
-//	 * @return user modified NOTA: Si no existe el constructor vacío de User, da un
-//	 *         error y el userRequest viene null.
-//	 */
-//	@PUT
-//	@Path("/updateUser")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response updateUser(Usuario userUpdate) {
-//		Usuario found = null;
-//		for (int i = 0; i < listaUsuarios.size(); i++) {
-//			if (listaUsuarios.get(i).getNombre().equalsIgnoreCase(userUpdate.getNombre())) {
-//				found = listaUsuarios.get(i);
-//			}
-//		}
-//
-//		if (found == null) {
-//			return Response.status(Status.BAD_REQUEST).entity("User not found").build();
-//		} else {
-//			found.setNombre(userUpdate.getNombre());
-//			return Response.ok(found).build();
-//		}
-//	}
-//
-//	/**
-//	 * URL: http://localhost:8080/API_REST_WS-RS/api/users/deleteUser/Rosa
-//	 * 
-//	 * @param User
-//	 * @return Response
-//	 */
-//	@DELETE
-//	@Path("/deleteUser/{name}")
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response deleteUser(@PathParam("name") String name) {
-//		Usuario found = null;
-//		for (int i = 0; i < listaUsuarios.size(); i++) {
-//			if (listaUsuarios.get(i).getNombre().equalsIgnoreCase(name)) {
-//				found = listaUsuarios.get(i);
-//				listaUsuarios.remove(found);
-//			}
-//		}
-//
-//		
+//		List<Share> listaGastos = gastoBean.findAllByIdPropietario(grupo.getIdPropietario());
+//		return Response.ok(listaGastos).build();
 //	}
 
 }
